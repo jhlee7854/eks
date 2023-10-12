65f169b28731b541bc455da528742b197e189c12
jonghak-dev <jonghak.dev@gmail.com>
2023-10-12 18:09:51 +0900

    Modify eks node group to use launch template

--- eks-node-group.tf
+++ eks-node-group.tf
@@ -21,27 +21,27 @@ resource "aws_iam_role_policy" "iam_role_policy_s3" {
     Version = "2012-10-17"
     Statement = [
       {
-        Sid = "MountpointFullBucketAccess"
+        Sid    = "MountpointFullBucketAccess"
         Effect = "Allow"
         Action = [
-            "s3:ListBucket"
+          "s3:ListBucket"
         ]
         Resource = [
-            "arn:aws:s3:::eks-share"
+          "arn:aws:s3:::eks-share"
         ]
       },
       {
-          Sid = "MountpointFullObjectAccess"
-          Effect = "Allow"
-          Action = [
-              "s3:GetObject",
-              "s3:PutObject",
-              "s3:AbortMultipartUpload",
-              "s3:DeleteObject"
-          ]
-          Resource = [
-              "arn:aws:s3:::eks-share/*"
-          ]
+        Sid    = "MountpointFullObjectAccess"
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:AbortMultipartUpload",
+          "s3:DeleteObject"
+        ]
+        Resource = [
+          "arn:aws:s3:::eks-share/*"
+        ]
       }
     ]
   })
@@ -66,7 +66,7 @@ resource "aws_eks_node_group" "app_node_group" {
   cluster_name    = aws_eks_cluster.eks_cluster.name
   node_group_name = "app-node-group"
   node_role_arn   = aws_iam_role.iam_role_eks_node_group.arn
-  subnet_ids      = [
+  subnet_ids = [
     aws_subnet.private_subnet_01.id,
     aws_subnet.private_subnet_02.id
   ]
@@ -82,7 +82,7 @@ resource "aws_eks_node_group" "app_node_group" {
   }
 
   launch_template {
-    name = var.app_node_group_launch_template_name
+    name    = var.app_node_group_launch_template_name
     version = var.app_node_group_launch_template_version
   }
 
@@ -92,5 +92,6 @@ resource "aws_eks_node_group" "app_node_group" {
     aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
     aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
     aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
+    aws_iam_role_policy.iam_role_policy_s3
   ]
 }
--- network.tf
+++ network.tf
@@ -1,55 +1,55 @@
 resource "aws_vpc" "vpc" {
   assign_generated_ipv6_cidr_block = false
-  cidr_block = "10.1.0.0/16"
-  enable_dns_hostnames = true
-  enable_dns_support = true
-  instance_tenancy = "default"
+  cidr_block                       = "10.1.0.0/16"
+  enable_dns_hostnames             = true
+  enable_dns_support               = true
+  instance_tenancy                 = "default"
 }
 
 resource "aws_subnet" "public_subnet_01" {
   assign_ipv6_address_on_creation = false
-  availability_zone = "ap-northeast-2a"
-  cidr_block = "10.1.0.0/18"
-  map_public_ip_on_launch = true
-  vpc_id = aws_vpc.vpc.id
+  availability_zone               = "ap-northeast-2a"
+  cidr_block                      = "10.1.0.0/18"
+  map_public_ip_on_launch         = true
+  vpc_id                          = aws_vpc.vpc.id
   tags = {
-    "Name" = "${var.project_name}-${var.env}-p01"
+    "Name"                   = "${var.project_name}-${var.env}-p01"
     "kubernetes.io/role/elb" = "1"
   }
 }
 
 resource "aws_subnet" "public_subnet_02" {
   assign_ipv6_address_on_creation = false
-  availability_zone = "ap-northeast-2c"
-  cidr_block = "10.1.64.0/18"
-  map_public_ip_on_launch = true
-  vpc_id = aws_vpc.vpc.id
+  availability_zone               = "ap-northeast-2c"
+  cidr_block                      = "10.1.64.0/18"
+  map_public_ip_on_launch         = true
+  vpc_id                          = aws_vpc.vpc.id
   tags = {
-    "Name" = "${var.project_name}-${var.env}-p02"
+    "Name"                   = "${var.project_name}-${var.env}-p02"
     "kubernetes.io/role/elb" = "1"
   }
 }
 
 resource "aws_subnet" "private_subnet_01" {
   assign_ipv6_address_on_creation = false
-  availability_zone = "ap-northeast-2a"
-  cidr_block = "10.1.128.0/18"
-  map_public_ip_on_launch = false
-  vpc_id = aws_vpc.vpc.id
+  availability_zone               = "ap-northeast-2a"
+  cidr_block                      = "10.1.128.0/18"
+  map_public_ip_on_launch         = false
+  vpc_id                          = aws_vpc.vpc.id
   tags = {
-    "Name" = "${var.project_name}-${var.env}-i01"
+    "Name"                            = "${var.project_name}-${var.env}-i01"
     "kubernetes.io/role/internal-elb" = "1"
   }
 }
 
 resource "aws_subnet" "private_subnet_02" {
   assign_ipv6_address_on_creation = false
-  availability_zone = "ap-northeast-2c"
-  cidr_block = "10.1.192.0/18"
-  map_public_ip_on_launch = false
-  vpc_id = aws_vpc.vpc.id
+  availability_zone               = "ap-northeast-2c"
+  cidr_block                      = "10.1.192.0/18"
+  map_public_ip_on_launch         = false
+  vpc_id                          = aws_vpc.vpc.id
   tags = {
-    "Name" = "${var.project_name}-${var.env}-i02"
+    "Name"                            = "${var.project_name}-${var.env}-i02"
     "kubernetes.io/role/internal-elb" = "1"
   }
 }
@@ -60,7 +60,7 @@ resource "aws_internet_gateway" "internet_gateway" {
 
 resource "aws_eip" "eip_01" {
   public_ipv4_pool = "amazon"
-  domain = "vpc"
+  domain           = "vpc"
   tags = {
     "Name" = "${var.project_name}-${var.env}-eip01"
   }
@@ -68,7 +68,7 @@ resource "aws_eip" "eip_01" {
 
 resource "aws_eip" "eip_02" {
   public_ipv4_pool = "amazon"
-  domain = "vpc"
+  domain           = "vpc"
   tags = {
     "Name" = "${var.project_name}-${var.env}-eip02"
   }
@@ -114,7 +114,7 @@ resource "aws_route_table" "public_route_table" {
 resource "aws_route_table" "private_route_table_01" {
   vpc_id = aws_vpc.vpc.id
   route {
-    cidr_block = "0.0.0.0/0"
+    cidr_block     = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.nat_gateway_01.id
   }
   tags = {
@@ -125,7 +125,7 @@ resource "aws_route_table" "private_route_table_01" {
 resource "aws_route_table" "private_route_table_02" {
   vpc_id = aws_vpc.vpc.id
   route {
-    cidr_block = "0.0.0.0/0"
+    cidr_block     = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.nat_gateway_02.id
   }
   tags = {
@@ -134,27 +134,27 @@ resource "aws_route_table" "private_route_table_02" {
 }
 
 resource "aws_route_table_association" "route_table_association_p01" {
-  subnet_id = aws_subnet.public_subnet_01.id
+  subnet_id      = aws_subnet.public_subnet_01.id
   route_table_id = aws_route_table.public_route_table.id
 }
 
 resource "aws_route_table_association" "route_table_association_p02" {
-  subnet_id = aws_subnet.public_subnet_02.id
+  subnet_id      = aws_subnet.public_subnet_02.id
   route_table_id = aws_route_table.public_route_table.id
 }
 
 resource "aws_route_table_association" "route_table_association_i01" {
-  subnet_id = aws_subnet.private_subnet_01.id
+  subnet_id      = aws_subnet.private_subnet_01.id
   route_table_id = aws_route_table.private_route_table_01.id
 }
 
 resource "aws_route_table_association" "route_table_association_i02" {
-  subnet_id = aws_subnet.private_subnet_02.id
+  subnet_id      = aws_subnet.private_subnet_02.id
   route_table_id = aws_route_table.private_route_table_02.id
 }
 
 resource "aws_security_group" "cluster_sg" {
-  name = "cluster-sg"
+  name        = "cluster-sg"
   description = "Communication between the control plane and worker nodegroups"
-  vpc_id = aws_vpc.vpc.id
+  vpc_id      = aws_vpc.vpc.id
 }
