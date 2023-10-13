f62841e3e193612410b4d06854e60e73d845e6d6
jonghak-dev <jonghak.dev@gmail.com>
2023-10-13 17:35:25 +0900

    Fix bugs

--- eks-node-group.tf
+++ eks-node-group.tf
@@ -89,6 +89,9 @@ resource "aws_launch_template" "app_node" {
   vpc_security_group_ids = [aws_security_group.cluster_sg.id]
   tag_specifications {
     resource_type = "instance"
+    tags = {
+      "Name" = "app-node"
+    }
   }
   lifecycle {
     create_before_destroy = true
