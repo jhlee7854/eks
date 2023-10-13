b09037e108ff6ef0259d0e601048f812731e0917
jonghak-dev <jonghak.dev@gmail.com>
2023-10-13 18:03:06 +0900

    Modify eks node group to use launch template

--- eks-node-group.tf
+++ eks-node-group.tf
@@ -85,7 +85,7 @@ resource "aws_launch_template" "app_node" {
   key_name               = "mountpoint-s3"
   name                   = "app_node_launch_template"
   image_id               = "ami-09af799f87c7601fa"
-  user_data              = base64encode(local.eks-app-node-userdata)
+  # user_data              = base64encode(local.eks-app-node-userdata)
   vpc_security_group_ids = [aws_security_group.cluster_sg.id]
   tag_specifications {
     resource_type = "instance"
