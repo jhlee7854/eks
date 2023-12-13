8e4b716169f504590467d86ad3610ed40e47aea7
jonghak-dev <jonghak.dev@gmail.com>
2023-12-13 15:02:32 +0900

    Fix eks idp bug

--- eks-idp.tf
+++ eks-idp.tf
@@ -14,6 +14,6 @@ data "tls_certificate" "eks_cluster" {
 
 resource "aws_iam_openid_connect_provider" "eks_cluster" {
   client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = [data.tls_certificate.example.certificates[0].sha1_fingerprint]
+  thumbprint_list = [data.tls_certificate.eks_cluster.certificates[0].sha1_fingerprint]
   url             = aws_eks_cluster.eks_cluster.identity[0].oidc[0].issuer
 }
