eedcadd5cc41289ce5702363b98ec954e0142241
jonghak-dev <jonghak.dev@gmail.com>
2023-12-13 21:41:54 +0900

    Modify eks addons

--- eks-addon-aws-s3-csi-driver.tf
+++ eks-addon-aws-s3-csi-driver.tf
@@ -70,6 +70,7 @@ resource "aws_eks_addon" "aws-mountpoint-s3-csi-driver" {
   cluster_name                = aws_eks_cluster.eks_cluster.name
   addon_name                  = "aws-mountpoint-s3-csi-driver"
   addon_version               = "v1.1.0-eksbuild.1"
+  service_account_role_arn    = aws_iam_role.AmazonEKSS3CSIDriverRole.arn
   resolve_conflicts_on_create = "OVERWRITE"
   resolve_conflicts_on_update = "PRESERVE"
   preserve                    = true
