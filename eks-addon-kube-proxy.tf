9f0ce4e4905506f825452dc0db93c6fecd15eb95
jonghak-dev <jonghak.dev@gmail.com>
2023-12-13 19:12:45 +0900

    Modify eks addons

--- eks-addon-kube-proxy.tf
+++ eks-addon-kube-proxy.tf
@@ -1,5 +1,5 @@
 resource "aws_eks_addon" "kube_proxy" {
   cluster_name  = aws_eks_cluster.eks_cluster.name
   addon_name    = "kube-proxy"
-  addon_version = "v1.27.1-eksbuild.1"
+  addon_version = "v1.27.6-eksbuild.2"
 }
